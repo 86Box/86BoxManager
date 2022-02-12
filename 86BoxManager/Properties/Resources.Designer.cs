c6232202c1c2f96037520ee60b35e99e486331d2
David Simunič <simunic.david@outlook.com>
2022-02-12 18:22:44 +0100

    Final icon clean up

--- 86BoxManager/Properties/Resources.Designer.cs
+++ 86BoxManager/Properties/Resources.Designer.cs
@@ -59,15 +59,5 @@ namespace _86boxManager.Properties {
                 resourceCulture = value;
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap _86Box {
-            get {
-                object obj = ResourceManager.GetObject("_86Box", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
