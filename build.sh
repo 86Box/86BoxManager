b800e8160698ea74357e34a23337dccda16d1e2c
xafero <georg.berendt@outlook.com>
2023-05-28 18:22:11 +0200

    Adding zipping to build

--- build.sh
+++ build.sh
@@ -3,3 +3,7 @@ mkdir dist
 dotnet publish 86BoxManager -r win-x64   -c Release --self-contained true -o dist/win
 dotnet publish 86BoxManager -r linux-x64 -c Release --self-contained true -o dist/lin
 dotnet publish 86BoxManager -r osx-x64   -c Release --self-contained true -o dist/osx
+mkdir pub
+tar -czf pub/86BoxManager_win.tar.gz   dist/win
+tar -czf pub/86BoxManager_linux.tar.gz dist/lin
+tar -czf pub/86BoxManager_mac.tar.gz   dist/osx
