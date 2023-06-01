7f200471a9e193189390a780b04f5a6dfd7e8673
xafero <georg.berendt@outlook.com>
2023-06-01 17:29:08 +0200

    Changed Windows build to be zip

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ dotnet publish 86BoxManager -r win-x64   -c Release --self-contained true -o dis
 dotnet publish 86BoxManager -r linux-x64 -c Release --self-contained true -o dist/lin
 dotnet publish 86BoxManager -r osx-x64   -c Release --self-contained true -o dist/osx
 mkdir pub
-tar -czf pub/86BoxManager_win.tar.gz   dist/win
+zip -q -r pub/86BoxManager_win.zip     dist/win
 tar -czf pub/86BoxManager_linux.tar.gz dist/lin
 tar -czf pub/86BoxManager_mac.tar.gz   dist/osx
