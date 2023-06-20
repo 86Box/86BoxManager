de6af4e4059ef16560cce5d88c40fcf8ef799818
xafero <georg.berendt@outlook.com>
2023-06-20 03:15:43 +0200

    Removed second build script

--- build.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-mkdir dist
-dotnet publish 86BoxManager -r win-x64   -c Release --self-contained true -o dist\win
-dotnet publish 86BoxManager -r linux-x64 -c Release --self-contained true -o dist\lin
-dotnet publish 86BoxManager -r osx-x64   -c Release --self-contained true -o dist\osx
